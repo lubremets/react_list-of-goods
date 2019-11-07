a0197c4db93a72da3308c4a188f5b6d2e42ebece
lubremets <lubremets@gmail.com>
2019-11-07 19:41:54 +0200

    Updates

---
+++ precache-manifest.fdb4f579a6263172758d3cfdec4244cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfdce9cb4d4f98423a98aecf1bfcc51",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "5592868e4d386bc2dec6",
+    "url": "/react_list-of-goods/static/css/main.09579e80.chunk.css"
+  },
+  {
+    "revision": "e708bed1a49773a4f0c3",
+    "url": "/react_list-of-goods/static/js/2.e17ad3c2.chunk.js"
+  },
+  {
+    "revision": "5592868e4d386bc2dec6",
+    "url": "/react_list-of-goods/static/js/main.aa33c1bb.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
